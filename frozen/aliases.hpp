93e474629327d65fab3e20f806b056d2b09ce795
rhysd <lin90162@yahoo.co.jp>
2013-08-11 00:53:57 +0900

    add header which includes all other headers and separate aliases

---
+++ frozen/aliases.hpp
@@ -0,0 +1,27 @@
+#if !defined FROZEN_ALIASES_HPP_INCLUDED
+#define      FROZEN_ALIASES_HPP_INCLUDED
+
+#include <cstddef>
+
+#include "./basic_string.hpp"
+
+namespace frozen {
+
+using std::size_t;
+
+template<size_t N>
+using string = basic_string<char, N>;
+
+template<size_t N>
+using wstring = basic_string<wchar_t, N>;
+
+template<size_t N>
+using w16string = basic_string<char16_t, N>;
+
+template<size_t N>
+using w32string = basic_string<char32_t, N>;
+
+} // namespace frozen
+
+
+#endif    // FROZEN_ALIASES_HPP_INCLUDED
--- frozen/string.hpp
+++ frozen/string.hpp
@@ -1,31 +1,11 @@
 #if !defined FROZEN_STRING_HPP_INCLUDED
 #define      FROZEN_STRING_HPP_INCLUDED
 
-#include <cstddef>
-
 #include "./basic_string.hpp"
+#include "./aliases.hpp"
 #include "./make.hpp"
 #include "./to_string.hpp"
 #include "./empty.hpp"
 #include "./literal.hpp"
-
-namespace frozen {
-
-using std::size_t;
-
-template<size_t N>
-using string = basic_string<char, N>;
-
-template<size_t N>
-using wstring = basic_string<wchar_t, N>;
-
-template<size_t N>
-using w16string = basic_string<char16_t, N>;
-
-template<size_t N>
-using w32string = basic_string<char32_t, N>;
-
-} // namespace frozen
-
 
 #endif    // FROZEN_STRING_HPP_INCLUDED
