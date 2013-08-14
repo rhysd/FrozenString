93b0e3cb5d81e99e3b5819446bf20e2fc02b002d
rhysd <lin90162@yahoo.co.jp>
2013-08-14 22:12:01 +0900

    add aliases for std::is_*

--- frozen/type_traits_aliases.hpp
+++ frozen/type_traits_aliases.hpp
@@ -29,6 +29,7 @@ namespace alias {
     template <class T>
     using add_volatile = typename std::add_volatile<T>::type;
 
+
     // 20.9.7.2 Reference modifications
     template <class T>
     using remove_reference = typename std::remove_reference<T>::type;
@@ -52,6 +53,7 @@ namespace alias {
 
     template <class T>
     using add_pointer = typename std::add_pointer<T>::type;
+
 
     // 20.9.7.6 Other transformations
     template <std::size_t Len, std::size_t Align>
@@ -79,10 +81,193 @@ namespace alias {
     template <class F, class... Args >
     using result_of = typename std::result_of<F(Args...)>::type;
 
+
+    // Primary categories
+    template<class T>
+    using is_void = typename std::is_void<T>::type;
+
+    template<class T>
+    using is_floating_point = typename std::is_floating_point<T>::type;
+
+    template<class T>
+    using is_array = typename std::is_array<T>::type;
+
+    template<class T>
+    using is_enum = typename std::is_enum<T>::type;
+
+    template<class T>
+    using is_union = typename std::is_union<T>::type;
+
+    template<class T>
+    using is_class = typename std::is_class<T>::type;
+
+    template<class T>
+    using is_function = typename std::is_function<T>::type;
+
+    template<class T>
+    using is_pointer = typename std::is_pointer<T>::type;
+
+    template<class T>
+    using is_lvalue_reference = typename std::is_lvalue_reference<T>::type;
+
+    template<class T>
+    using is_rvalue_reference = typename std::is_rvalue_reference<T>::type;
+
+    template<class T>
+    using is_member_object_pointer = typename std::is_member_object_pointer<T>::type;
+
+    template<class T>
+    using is_member_function_pointer = typename std::is_member_function_pointer<T>::type;
+
+
+    // Composite type categories
+    template<class T>
+    using is_fundamental = typename std::is_fundamental<T>::type;
+
+    template<class T>
+    using is_arithmetic = typename std::is_arithmetic<T>::type;
+
+    template<class T>
+    using is_scalar = typename std::is_scalar<T>::type;
+
+    template<class T>
+    using is_object = typename std::is_object<T>::type;
+
+    template<class T>
+    using is_compound = typename std::is_compound<T>::type;
+
+    template<class T>
+    using is_reference = typename std::is_reference<T>::type;
+
+    template<class T>
+    using is_member_pointer = typename std::is_member_pointer<T>::type;
+
+
+    // Type properties
+    template<class T>
+    using is_const = typename std::is_const<T>::type;
+
+    template<class T>
+    using is_volatile = typename std::is_volatile<T>::type;
+
+    template<class T>
+    using is_trivial = typename std::is_trivial<T>::type;
+
+    // gcc-4.8.1 seems not to implement is_trivially_copyable
+    // template<class T>
+    // using is_trivially_copyable = typename std::is_trivially_copyable<T>::type;
+
+    template<class T>
+    using is_standard_layout = typename std::is_standard_layout<T>::type;
+
+    template<class T>
+    using is_pod = typename std::is_pod<T>::type;
+
+    template<class T>
+    using is_literal_type = typename std::is_literal_type<T>::type;
+
+    template<class T>
+    using is_empty = typename std::is_empty<T>::type;
+
+    template<class T>
+    using is_polymorphic = typename std::is_polymorphic<T>::type;
+
+    template<class T>
+    using is_abstract = typename std::is_abstract<T>::type;
+
+    template<class T>
+    using is_signed = typename std::is_signed<T>::type;
+
+    template<class T>
+    using is_unsigned = typename std::is_unsigned<T>::type;
+
+
+    // Supported operations
+    template<class T>
+    using is_constructible = typename std::is_constructible<T>::type;
+
+    // template<class T>
+    // using is_trivially_constructible = typename std::is_trivially_constructible<T>::type;
+
+    template<class T>
+    using is_nothrow_constructible = typename std::is_nothrow_constructible<T>::type;
+
+    template<class T>
+    using is_default_constructible = typename std::is_default_constructible<T>::type;
+
+    // template<class T>
+    // using is_trivially_default_constructible = typename std::is_trivially_default_constructible<T>::type;
+
+    template<class T>
+    using is_nothrow_default_constructible = typename std::is_nothrow_default_constructible<T>::type;
+
+    template<class T>
+    using is_copy_constructible = typename std::is_copy_constructible<T>::type;
+
+    // template<class T>
+    // using is_trivially_copy_constructible = typename std::is_trivially_copy_constructible<T>::type;
+
+    template<class T>
+    using is_nothrow_copy_constructible = typename std::is_nothrow_copy_constructible<T>::type;
+
+    template<class T>
+    using is_move_constructible = typename std::is_move_constructible<T>::type;
+
+    // template<class T>
+    // using is_trivially_move_constructible = typename std::is_trivially_move_constructible<T>::type;
+
+    template<class T>
+    using is_nothrow_move_constructible = typename std::is_nothrow_move_constructible<T>::type;
+
+    template<class T, class U>
+    using is_assignable = typename std::is_assignable<T, U>::type;
+
+    // template<class T, class U>
+    // using is_trivially_assignable = typename std::is_trivially_assignable<T, U>::type;
+
+    template<class T, class U>
+    using is_nothrow_assignable = typename std::is_nothrow_assignable<T, U>::type;
+
+    template<class T>
+    using is_copy_assignable = typename std::is_copy_assignable<T>::type;
+
+    // template<class T>
+    // using is_trivially_copy_assignable = typename std::is_trivially_copy_assignable<T>::type;
+
+    template<class T>
+    using is_nothrow_copy_assignable = typename std::is_nothrow_copy_assignable<T>::type;
+
+    template<class T>
+    using is_move_assignable = typename std::is_move_assignable<T>::type;
+
+    // template<class T>
+    // using is_trivially_move_assignable = typename std::is_trivially_move_assignable<T>::type;
+
+    template<class T>
+    using is_nothrow_move_assignable = typename std::is_nothrow_move_assignable<T>::type;
+
+    template<class T>
+    using is_destructible = typename std::is_destructible<T>::type;
+
+    // template<class T>
+    // using is_trivially_destructible = typename std::is_trivially_destructible<T>::type;
+
+    template<class T>
+    using is_nothrow_destructible = typename std::is_nothrow_destructible<T>::type;
+
+    template<class T>
+    using has_virtual_destructor = typename std::has_virtual_destructor<T>::type;
+
+
+    // Type relationships
     template<class T, class U>
     using is_same = typename std::is_same<T, U>::type;
 
-    
+    template<class B, class D>
+    using is_base_of = typename std::is_base_of<B, D>::type;
+
+    template<class F, class T>
+    using is_convertible = typename std::is_convertible<F, T>::type;
 
 } // namespace alias
 } // namespace frozen
