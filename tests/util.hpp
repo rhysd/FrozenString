4dc7258158f29233368f1035ba07009ae80b016d
rhysd <lin90162@yahoo.co.jp>
2013-08-17 02:39:38 +0900

    macro to check if an expression is constant expression or not

--- tests/util.hpp
+++ tests/util.hpp
@@ -4,4 +4,14 @@
 #define STATIC_ASSERT(expr) static_assert(expr, #expr)
 #define STATIC_ASSERT_NOT(expr) static_assert(!(expr), #expr)
 
+namespace test_frozen{
+template<class T>
+constexpr T is_constexpr_impl(T x) throw(int)
+{
+  return x;
+}
+}
+
+#define IS_CONSTEXPR(EXPR) noexcept(test_frozen::is_constexpr_impl(EXPR))
+
 #endif    // ISTRING_TEST_UTIL_HPP_INCLUDED
