d299bf6b91d382bcddac84f29e301f0abc7d8bd7
rhysd <lin90162@yahoo.co.jp>
2013-09-06 00:24:14 +0900

    add a header to include all other headers

---
+++ frozen/type/string.hpp
@@ -0,0 +1,9 @@
+#if !defined FROZEN_TYPE_STRING_HPP_INCLUDED
+#define      FROZEN_TYPE_STRING_HPP_INCLUDED
+
+#include "./basic_string.hpp"
+#include "string_aliases.hpp"
+#include "./algorithm.hpp"
+#include "from_string_literal.hpp"
+
+#endif    // FROZEN_TYPE_STRING_HPP_INCLUDED
