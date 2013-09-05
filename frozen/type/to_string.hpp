eb983a192e7032275db27dc8a01c75ef9c065d5c
rhysd <lin90162@yahoo.co.jp>
2013-09-06 01:21:30 +0900

    change aliases to ::type and add macro to deduce type

--- frozen/type/to_string.hpp
+++ frozen/type/to_string.hpp
@@ -32,28 +32,37 @@ namespace detail {
 
 } // namespace detail
 
-template<class CharT, class T, T Value>
-struct to_basic_string;
-
-template<class CharT, class Int, typename std::is_integral<Int>::type Value>
-struct to_basic_string<CharT, Int, Value> : detail::to_string_from_integer<CharT, Int, Value>
+template<class CharT, class Int, Int Value>
+struct to_basic_string : detail::to_string_from_integer<CharT, Int, Value>
 {
     static_assert(std::is_integral<Int>::value, "");
 };
 
 template<class Int, Int Value>
-using to_string = to_basic_string<char, Int, Value>;
+using to_string = typename to_basic_string<char, Int, Value>::type;
 
 template<class Int, Int Value>
-using to_wstring = to_basic_string<wchar_t, Int, Value>;
+using to_wstring = typename to_basic_string<wchar_t, Int, Value>::type;
 
 template<class Int, Int Value>
-using to_u16string = to_basic_string<char16_t, Int, Value>;
+using to_u16string = typename to_basic_string<char16_t, Int, Value>::type;
 
 template<class Int, Int Value>
-using to_u32string = to_basic_string<char32_t, Int, Value>;
+using to_u32string = typename to_basic_string<char32_t, Int, Value>::type;
 
 } // namespace type
 } // namespace frozen
+
+#define FROZEN_TO_STRING(INT) \
+    typename frozen::type::to_string<typename std::decay<decltype(INT)>::type, INT>
+
+#define FROZEN_TO_WSTRING(INT) \
+    typename frozen::type::to_wstring<typename std::decay<decltype(INT)>::type, INT>
+
+#define FROZEN_TO_U16STRING(INT) \
+    typename frozen::type::to_u16string<typename std::decay<decltype(INT)>::type, INT>
+
+#define FROZEN_TO_U32STRING(INT) \
+    typename frozen::type::to_u32string<typename std::decay<decltype(INT)>::type, INT>
 
 #endif    // FROZEN_TYPE_TO_STRING_HPP_INCLUDED
