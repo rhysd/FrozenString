84121391327d55bf3302392b5a3a2ccf0db43256
rhysd <lin90162@yahoo.co.jp>
2013-09-06 02:16:35 +0900

    add boost dependency for Travis CI

--- tests/util.hpp
+++ tests/util.hpp
@@ -1,9 +1,6 @@
 #if !defined ISTRING_TEST_UTIL_HPP_INCLUDED
 #define      ISTRING_TEST_UTIL_HPP_INCLUDED
 
-#include "../frozen/string.hpp"
-#include "../frozen/type/string.hpp"
-
 #define SASSERT(...) FROZEN_STATIC_ASSERT(__VA_ARGS__)
 #define SASSERT_NOT(...) FROZEN_STATIC_ASSERT_NOT(__VA_ARGS__)
 
