b96f21b2bb5cc1c2a82aeb857260cff8a6b2f804
rhysd <lin90162@yahoo.co.jp>
2013-09-06 00:01:37 +0900

    add basic_string and string aliases for each char type

--- frozen/type/basic_string.hpp
+++ frozen/type/basic_string.hpp
@@ -1,1 +1,36 @@
-// Not implemented yet.
+#if !defined FROZEN_TYPE_BASIC_STRING_HPP_INCLUDED
+#define      FROZEN_TYPE_BASIC_STRING_HPP_INCLUDED
+
+#include <cstddef>
+#include <array>
+#include <iostream>
+#include <type_traits>
+
+#include <boost/preprocessor/repetition/enum.hpp>
+#include <boost/preprocessor/facilities/intercept.hpp>
+#include <boost/preprocessor/config/limits.hpp>
+
+namespace frozen {
+namespace type {
+
+using std::size_t;
+
+template< class CharT, CharT... Chars >
+struct basic_string{
+
+    static const CharT value[sizeof...(Chars)+1];
+
+    constexpr std::array<CharT, sizeof...(Chars)+1>
+    to_array() const
+    {
+        return {{Chars..., '\0'}};
+    }
+};
+
+template< class CharT, CharT... Chars >
+const CharT basic_string<CharT, Chars...>::value[sizeof...(Chars)+1] = {Chars..., '\0'};
+
+} // namespace type
+} // namespace frozen
+
+#endif    // FROZEN_TYPE_BASIC_STRING_HPP_INCLUDED
---
+++ frozen/type/string_aliases.hpp
@@ -0,0 +1,24 @@
+#if !defined FROZEN_TYPE_STRING_ALIASES_HPP_INCLUDED
+#define      FROZEN_TYPE_STRING_ALIASES_HPP_INCLUDED
+
+#include "./basic_string.hpp"
+
+namespace frozen {
+namespace type {
+
+template<char... Chars>
+using string = basic_string<char, Chars...>;
+
+template<wchar_t... Chars>
+using wstring = basic_string<wchar_t, Chars...>;
+
+template<char16_t... Chars>
+using u16string = basic_string<char16_t, Chars...>;
+
+template<char32_t... Chars>
+using u32string = basic_string<char32_t, Chars...>;
+
+} // namespace type
+} // namespace frozen
+
+#endif    // FROZEN_TYPE_STRING_ALIASES_HPP_INCLUDED
