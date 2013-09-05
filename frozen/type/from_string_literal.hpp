dfd455a5cab1d4cd9ba588c530122a36fc074117
rhysd <lin90162@yahoo.co.jp>
2013-09-06 01:20:40 +0900

    add mising min() and fix algorithm function name

--- frozen/type/from_string_literal.hpp
+++ frozen/type/from_string_literal.hpp
@@ -10,15 +10,30 @@
 #include <boost/preprocessor/facilities/intercept.hpp>
 #include <boost/preprocessor/config/limits.hpp>
 
+namespace frozen {
+namespace type {
+namespace detail {
+
+    template<class T>
+    constexpr T min(T a, T b)
+    {
+        return a < b ? a : b;
+    }
+
+} // namespace detail
+} // namespace type
+} // namespace frozen
+
+
 #define FROZEN_FROM_STRING_LITERAL(lit) FROZEN_FROM_STRING_LITERAL_I(lit)
 #define FROZEN_FROM_STRING_LITERAL_I(lit) \
-    remove_trailing_nuls< \
+    frozen::type::remove_trailing_nuls< \
         sizeof(lit) / sizeof(*lit), \
         frozen::type::basic_string< \
             std::decay<decltype(*lit)>::type, \
             BOOST_PP_ENUM(BOOST_PP_LIMIT_REPEAT, FROZEN_FROM_STRING_LITERAL_M, lit) \
         > \
     >
-#define FROZEN_FROM_STRING_LITERAL_M(z, i, lit) lit[min((size_t)i, sizeof(lit) / sizeof(*lit) - 1)]
+#define FROZEN_FROM_STRING_LITERAL_M(z, i, lit) lit[frozen::type::detail::min((size_t)i, sizeof(lit) / sizeof(*lit) - 1)]
 
 #endif    // FROZEN_TYPE_FROM_STRING_LITERAL_HPP_INCLUDED
