4892b7c0008dd2db17c60a3070e8857e7b1a73f9
rhysd <lin90162@yahoo.co.jp>
2013-09-07 11:02:52 +0900

    add integrated header for the environment boost is not installed

---
+++ frozen/meta/string_without_boost.hpp
@@ -0,0 +1,9 @@
+#if !defined FROZEN_META_STRING_WITHOUT_BOOST_HPP_INCLUDED
+#define      FROZEN_META_STRING_WITHOUT_BOOST_HPP_INCLUDED
+
+#include "./string/basic_string.hpp"
+#include "./string/string_aliases.hpp"
+#include "./string/algorithm.hpp"
+#include "./string/to_string.hpp"
+
+#endif    // FROZEN_META_STRING_WITHOUT_BOOST_HPP_INCLUDED
