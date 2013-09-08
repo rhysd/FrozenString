6d38f48ad4e5d345056f1e20b11e4f490c9c1b06
rhysd <lin90162@yahoo.co.jp>
2013-09-09 03:01:25 +0900

    'add boost::mpl::size support'

--- frozen/meta/string/boost/mpl_compatibility.hpp
+++ frozen/meta/string/boost/mpl_compatibility.hpp
@@ -1,6 +1,48 @@
 #if !defined FROZEN_META_STRING_BOOST_MPL_COMPATIBILITY_HPP_INCLUDED
 #define      FROZEN_META_STRING_BOOST_MPL_COMPATIBILITY_HPP_INCLUDED
 
+#include <boost/mpl/size_t.hpp>
+#include <boost/mpl/size.hpp>
 
+namespace frozen {
+namespace meta {
+
+template< class CharT, CharT... Chars >
+struct basic_string;
+
+struct string_tag;
+struct string_iterator_tag;
+
+} // namespace meta
+} // namespace frozen
+
+namespace boost {
+namespace mpl {
+
+template<class Sequence>
+struct sequence_tag;
+
+template<class CharT, CharT... Chars>
+struct sequence_tag<frozen::meta::basic_string<CharT, Chars...>>{
+    using type = frozen::meta::string_tag;
+};
+
+template<class Tag>
+struct size_impl;
+
+template<>
+struct size_impl<frozen::meta::string_tag>{
+    template<class String>
+    struct apply;
+
+    template<class CharT, CharT... Chars>
+    struct apply<frozen::meta::basic_string<CharT, Chars...>>
+        : mpl::size_t<sizeof...(Chars)>
+    {};
+};
+
+
+} // namespace mpl
+} // namespace boost
 
 #endif    // FROZEN_META_STRING_BOOST_MPL_COMPATIBILITY_HPP_INCLUDED
